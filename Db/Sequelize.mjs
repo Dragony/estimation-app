a38c6b23b22673931cc8fdc87b5ffde2abf05202
Mattias Gysin <mattias.gysin@wasd.ch>
2017-12-03 21:51:33 +0100

    Swapped directory structure of client and server

--- Db/Sequelize.mjs
+++ Db/Sequelize.mjs
@@ -1,12 +1,13 @@
 import Sequelize from 'sequelize';
 
+let sequelize;
 if(process.env.CLEARDB_DATABASE_URL){
-    const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
         dialect: 'mysql',
         operatorsAliases: false
     });
 }else{
-    const sequelize = new Sequelize('estimations', 'root', '', {
+    sequelize = new Sequelize('estimations', 'root', '', {
         host: 'localhost',
         dialect: 'mysql',
         operatorsAliases: false
--- index.mjs
+++ index.mjs
@@ -1,16 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import uuid from 'uuid/v4';
+import ejs from 'ejs';
 import { handleGetRequest, handlePostRequest, handleEstimationDeleteRequest } from './Page/EstimationPage.mjs';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static('client/build'));
+app.set('views', 'client/build');
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
 
+console.log(process.argv[1] + '/client/build/index.html');
+
+app.get('/', (req, res) => res.redirect(302, '/' + uuid()));
 app.get('/api/new', (req, res) => res.redirect(302, '/api/' + uuid()));
 app.get('/favicon.ico', (req, res) => res.send('Nothing here'));
 app.post('/api/delete-estimation', handleEstimationDeleteRequest);
 app.get('/api/:uuid', handleGetRequest);
 app.post('/api/:uuid', handlePostRequest);
+app.get('/:uuid', (req, res) => res.render('index'));
 
 app.listen(3001, () => console.log('running!'));
