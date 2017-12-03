2e37f08fa2e16ac133770d8f2778ba2fd8dc5454
Mattias Gysin <mattias.gysin@wasd.ch>
2017-12-03 20:55:56 +0100

    Changes for heroku

--- server/Db/Sequelize.mjs
+++ server/Db/Sequelize.mjs
@@ -1,9 +1,16 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('estimations', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorsAliases: false
-});
+if(process.env.CLEARDB_DATABASE_URL){
+    const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+        dialect: 'mysql',
+        operatorsAliases: false
+    });
+}else{
+    const sequelize = new Sequelize('estimations', 'root', '', {
+        host: 'localhost',
+        dialect: 'mysql',
+        operatorsAliases: false
+    });
+}
 
 export default sequelize;
